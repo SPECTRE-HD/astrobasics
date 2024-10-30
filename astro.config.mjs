48bb5e18bcb2e051e186d6b5a0787bb5de1de866
Yves Thorrez <yves.thorrez@gmail.com>
2024-10-30 12:20:46 +0100

    home page editing for spectre project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://spectre-hd.github.io',
-    base: '/astrobasics'
+    site: 'https://spectre-hd.github.io'
 });
